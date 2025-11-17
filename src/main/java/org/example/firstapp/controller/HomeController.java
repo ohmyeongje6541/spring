543284e1bc06ae3259a6899738e19cdf0902acb7
ohmyeongje <tiqmtiqm6541@gmail.com>
2025-11-17 17:53:58 +0900

    복습문제

--- src/main/java/org/example/firstapp/controller/HomeController.java
+++ src/main/java/org/example/firstapp/controller/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDate;
 import java.time.Year;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -140,5 +141,119 @@ public class HomeController {
         model.addAttribute("title", title);
         model.addAttribute("content", content);
         return "pong";
+    }
+
+    @GetMapping("/greet/{name}")
+    public String greet(@PathVariable String name, Model model) {
+        model.addAttribute("name" , "name");
+        return "greet";
+    }
+
+    @GetMapping("/square/{num}")
+    public String square(@PathVariable int num, Model model) {
+        int result = num * num;
+        model.addAttribute("num", num);
+        model.addAttribute("result", result);
+        return "square";
+    }
+
+    @GetMapping("/food")
+    public String food(Model model) {
+        List<String> food = Arrays.asList("국밥", "탕수육", "돈까스", "제육볶음", "스테이크");
+        Random random = new Random();
+        String select = food.get(random.nextInt(food.size()));
+        model.addAttribute("select", select);
+        return "food";
+    }
+
+    @GetMapping("/today")
+    public String today(Model model) {
+        LocalDate day = LocalDate.now();
+        model.addAttribute("day", day);
+        return "today";
+    }
+
+    @GetMapping("/bmi")
+    public String bmi(
+            @RequestParam int weight,
+            @RequestParam int height,
+            Model model) {
+        double bmi = weight / Math.pow(height / 100, 2);
+        model.addAttribute("bmi", Math.round(bmi * 10) / 10.0);
+        return "bmi";
+    }
+
+    @GetMapping("/randomColor")
+    public String randomColor(Model model) {
+        List<String> color = Arrays.asList("red", "blue", "green", "yellow", "black");
+        Random random = new Random();
+        String list = color.get(random.nextInt(color.size()));
+        model.addAttribute("list", list);
+        return "randomColor";
+    }
+
+    @GetMapping("/grade2")
+    public String grade2(
+            @RequestParam int score,
+            Model model
+    ) {
+        String grade;
+        if (score >= 90) grade = "A";
+        else if (score >= 80) grade = "B";
+        else if (score >= 70) grade = "C";
+        else grade = "D";
+
+        model.addAttribute("score", score);
+        model.addAttribute("grade", grade);
+        return "grade2";
+    }
+
+    @GetMapping("/sum")
+    public String sum(
+            @RequestParam int num1,
+            @RequestParam int num2,
+            Model model
+    ) {
+        int sum = num1 + num2;
+        model.addAttribute("sum", sum);
+        return "sum";
+    }
+
+    @GetMapping("/lotto2")
+    public String lotto2(Model model) {
+        List<Integer> lotto = IntStream.rangeClosed(1, 45)
+                .boxed()
+                .collect(Collectors.toList());
+        Collections.shuffle(lotto);
+        List<Integer> lucky = lotto.subList(0, 6);
+        int bonus = lotto.get(6);
+        model.addAttribute("lucky", lucky);
+        model.addAttribute("bonus", bonus);
+        return "lotto2";
+    }
+
+    @GetMapping("/calc")
+    public String calc(
+            @RequestParam int x,
+            @RequestParam int y,
+            @RequestParam String op,
+            Model model) {
+        double result;
+
+        switch (op) {
+            case "add": result = x + y; break;
+            case "sub": result = x - y; break;
+            case "mul": result = x * y; break;
+            case "div":  result = (y == 0) ? 0 : (double)x / y; break;
+            default:
+                result = 0;
+        }
+
+        model.addAttribute("x", x);
+        model.addAttribute("y", y);
+        model.addAttribute("op", op);
+        model.addAttribute("result", result);
+
+        return "calc";
     }
 }
